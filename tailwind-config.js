4b6ca2644407372ea34414ae8e9a4938d64389c2
Curtis Papa Ankomah Poku <curtispapaankomahpoku@gmail.com>
2026-04-22 03:03:32 +0000

    added more colour!

---
+++ tailwind-config.js
@@ -0,0 +1,9 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                mint: { 400: '#4ade80', 500: '#22c55e', 600: '#16a34a' }
+            }
+        }
+    }
+}
