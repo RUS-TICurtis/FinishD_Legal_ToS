066fc96aede0c91bcf2bcc8d7976022f2478aae4
Curtis Papa Ankomah Poku <curtispapaankomahpoku@gmail.com>
2026-04-30 23:20:49 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -137,8 +137,7 @@ function App() {
                 <h3 className="text-4xl font-bold text-white">Fresh Trailers</h3>
                 <p className="text-on-surface-variant">Curated for your cinematic taste.</p>
               </div>
-              <button className="text-primary font-bold flex items-center gap-2 hover:opacity-80">
-              </button>
+              <button className="text-primary font-bold flex items-center gap-2 hover:opacity-80"></button>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
               {[
